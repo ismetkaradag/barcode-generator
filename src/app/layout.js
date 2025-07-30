c97c59cdc196c4ee5e276e7c512e283db16060cc
İsmet Karadağ <ismet.k3908@gmail.com>
2025-07-30 13:49:29 +0300

    og settings

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,17 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Barkod Oluşturucu',
   description: 'Excel veya manuel veri ile barkod oluşturma ve yazdırma uygulaması',
+  logo: '/logom.png',
+  icons: {
+    icon: '/logom.png',
+    shortcut: '/logom.png',
+    apple: '/logom.png',
+  },
+  openGraph: {
+    title: 'Barkod Oluşturucu',
+    description: 'Excel veya manuel veri ile barkod oluşturma ve yazdırma uygulaması',
+    logo: '/logom.png',
+  }
 }
 
 export default function RootLayout({ children }) {
